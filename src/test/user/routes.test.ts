f9e57dd69481dbd4793c5bd8efbd783303aeeb25
WojciechPaczula <wpaczula@future-processing.com>
2020-08-13 15:08:43 +0200

    test: Add registration tests

--- src/test/user/routes.test.ts
+++ src/test/user/routes.test.ts
@@ -4,15 +4,15 @@ import makeDBClient from '../factory/db-client';
 import { makeUser } from '../factory/user';
 import { Server } from 'http';
 import { expect } from 'chai';
+import { INewUser } from '../../features/user/models/auth';
 
 describe('user routes', () => {
+  let server: Server;
+
+  afterEach((done) => {
+    server.close(done);
+  });
   describe('GET users', () => {
-    let server: Server;
-
-    afterEach((done) => {
-      server.close(done);
-    });
-
     it('should return users from db.', async () => {
       const users = [makeUser({ id: 0 }), makeUser({ id: 1 })];
       const dbClient = makeDBClient({ queryRows: users });
@@ -25,6 +25,37 @@ describe('user routes', () => {
         .expect((res) => {
           expect(res.body).to.be.deep.equal(users);
         });
+    });
+  });
+
+  describe('POST register', () => {
+    it('should return 400 invalid error if user with a given email already exists.', async () => {
+      const email = 'email@email.com';
+      const users = [makeUser({ email })];
+      const dbClient = makeDBClient({ queryRows: users });
+      const newUser: INewUser = { email, password: 'password' };
+
+      server = await makeServer(dbClient);
+
+      return request(server)
+        .post('/register')
+        .send(newUser)
+        .expect(400)
+        .expect((res) => {
+          expect(res.body.message).to.be.equal('email is already used.');
+        });
+    });
+
+    it('should return 201 - created if user was successfuly created.', async () => {
+      const dbClient = makeDBClient({ queryRows: [] });
+      const newUser: INewUser = {
+        email: 'other-email@email.com',
+        password: 'password',
+      };
+
+      server = await makeServer(dbClient);
+
+      return request(server).post('/register').send(newUser).expect(201);
     });
   });
 });
