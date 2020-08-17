e7fa09258ddb63957f3d53c31e9c7b230dfe6ec2
WojciechPaczula <wpaczula@future-processing.com>
2020-08-17 13:17:22 +0200

    test: Add authentication tests

--- src/test/user/routes.test.ts
+++ src/test/user/routes.test.ts
@@ -5,7 +5,8 @@ import { makeUser, makeUserDto } from '../factory/user';
 import { Server } from 'http';
 import { expect } from 'chai';
 import { NewUser } from '../../features/user/models/auth';
-import { stub } from 'sinon';
+import { stub, SinonStub } from 'sinon';
+import { User } from '../../features/user/models/user';
 
 const authModule = require('../../features/user/middlewares/auth');
 stub(authModule, 'authenticated').callsFake((req, res, next) => next());
@@ -61,6 +62,59 @@ describe('user routes', () => {
       server = await makeServer(dbClient);
 
       return request(server).post('/register').send(newUser).expect(201);
+    });
+  });
+
+  describe('POST login', () => {
+    const hashModule = require('../../features/user/utils/hash');
+    let hashStub: SinonStub;
+
+    afterEach(() => {
+      hashStub.restore();
+    });
+
+    it('should return 204 and set session cookie if the user is successfully authenticated.', async () => {
+      hashStub = stub(hashModule, 'compareHash').returns(true);
+      const email = 'email@email.com';
+      const password = 'password';
+      const credentials = { email, password };
+      const users = [makeUser({ email, password })];
+      const dbClient = makeDBClient({ queryRows: users });
+
+      server = await makeServer(dbClient);
+
+      return request(server)
+        .post('/login')
+        .send(credentials)
+        .expect(204)
+        .expect((res) => {
+          expect(res.header['set-cookie']).not.to.be.empty;
+        });
+    });
+
+    it('should return 401 if the user provided wrong password.', async () => {
+      hashStub = stub(hashModule, 'compareHash').returns(false);
+      const email = 'email@email.com';
+      const credentials = { email, password: 'wrong-password' };
+      const users = [makeUser({ email })];
+      const dbClient = makeDBClient({ queryRows: users });
+
+      server = await makeServer(dbClient);
+
+      return request(server).post('/login').send(credentials).expect(401);
+    });
+
+    it('should return 401 if the user does not exist.', async () => {
+      const credentials = {
+        email: 'not-existing@email.com',
+        password: 'password',
+      };
+      const users: User[] = [];
+      const dbClient = makeDBClient({ queryRows: users });
+
+      server = await makeServer(dbClient);
+
+      return request(server).post('/login').send(credentials).expect(401);
     });
   });
 });
