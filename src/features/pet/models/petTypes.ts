a46f3a474acb797089ce14aed76400e79442be55
WojciechPaczula <wpaczula@future-processing.com>
2020-08-20 11:35:59 +0200

    feat: Add readonly for pet type properties

--- src/features/pet/models/petTypes.ts
+++ src/features/pet/models/petTypes.ts
@@ -1,11 +1,11 @@
 import { validateNewPetType } from '../validators/petTypes';
 
 export interface NewPetType {
-  name: string;
+  readonly name: string;
 }
 
 export interface PetType extends NewPetType {
-  id: number;
+  readonly id: number;
 }
 
 export const makePetType = async (name: string): Promise<NewPetType> => {
